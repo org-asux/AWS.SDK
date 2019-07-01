865785e6bceb467a21c493240ad3bd0f098321fd
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-06-30 21:12:22 -0400

    new --create-key-pair and --delete-key-pair commands

--- src/main/java/org/ASUX/AWSSDK/AWSSDK.java
+++ src/main/java/org/ASUX/AWSSDK/AWSSDK.java
@@ -32,6 +32,8 @@
 
 package org.ASUX.AWSSDK;
 
+import org.ASUX.yaml.JSONTools;
+
 // import java.util.Map;
 import java.util.LinkedList;
 import java.util.ArrayList;
@@ -39,6 +41,7 @@ import java.util.LinkedHashMap;
 import java.util.Properties;
 //import java.util.regex.*;
 import java.util.Set;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -66,8 +69,10 @@ import com.amazonaws.services.ec2.AmazonEC2Client;
     // https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/ec2/model/AvailabilityZone.html
 // import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
 // import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupIngressRequest;
-// import com.amazonaws.services.ec2.model.CreateKeyPairRequest;
-// import com.amazonaws.services.ec2.model.CreateKeyPairResult;
+import com.amazonaws.services.ec2.model.CreateKeyPairRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairResult;
+import com.amazonaws.services.ec2.model.DeleteKeyPairRequest;
+import com.amazonaws.services.ec2.model.DeleteKeyPairResult;
 // import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
 // import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 // import com.amazonaws.services.ec2.model.DescribeInstancesResult;
@@ -205,6 +210,7 @@ public class AWSSDK {
         // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-ec2-regions-zones.html
         // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
         final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withCredentials( this.AWSAuthenticationHndl ).withRegion( _regionStr==null?"us-east-2":_regionStr ).build();
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
         // To use the default credential/region provider chain 
         // Ec2Client ec2 = Ec2Client.create(); // AWS_REGION is checked .. ~/.aws/config default profile .. aws.profile system property
         return ec2;
@@ -220,6 +226,7 @@ public class AWSSDK {
      */
     public ArrayList<String> getRegions( ) {
         final AmazonEC2 ec2 = this.getAWSEC2Hndl( null );
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
         final DescribeRegionsResult regions_response = ec2.describeRegions();
         final ArrayList<String> retarr = new ArrayList<String>();
         for(Region region : regions_response.getRegions()) {
@@ -236,6 +243,7 @@ public class AWSSDK {
      */
     public ArrayList<String>  getAZs( final String _regionStr ) {
         final AmazonEC2 ec2 = this.getAWSEC2Hndl( _regionStr );
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
         DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
         final ArrayList<String> retarr = new ArrayList<String>();
         for(AvailabilityZone zone : zones_response.getAvailabilityZones()) {
@@ -253,6 +261,7 @@ public class AWSSDK {
      */
     public ArrayList< LinkedHashMap<String,Object> >  describeAZs( final String _regionStr ) throws Exception {
         final AmazonEC2 ec2 = this.getAWSEC2Hndl( _regionStr );
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
         DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
         final ArrayList< LinkedHashMap<String,Object> > retarr = new ArrayList<>();
 
@@ -302,6 +311,45 @@ public class AWSSDK {
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     //==============================================================================
 
+    public void deleteKeyPairEC2( final String _regionStr, final String _MySSHKeyName ) throws Exception {
+        // https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/java/example_code/ec2/src/main/java/aws/example/ec2/DeleteKeyPair.java
+        // http://docs.amazonaws.cn/en_us/sdk-for-java/v1/developer-guide/examples-ec2-key-pairs.html
+        final String HDR = CLASSNAME +"deleteKeyPairEC2("+ _regionStr +","+ _MySSHKeyName +"): ";
+        final AmazonEC2 ec2 = this.getAWSEC2Hndl( _regionStr );
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
+        final DeleteKeyPairRequest request = new DeleteKeyPairRequest().withKeyName( _MySSHKeyName );
+        final DeleteKeyPairResult response = ec2.deleteKeyPair(request);
+        if ( this.verbose) System.out.println( HDR +"DeleteKeyPairResult =\n"+ response.toString() );
+        if ( response == null ||    !  "{}".equals( response.toString().trim() ) )
+            throw new Exception( "FAILURE invoking AWS-SDK re: "+ HDR +"\nResponse="+response );
+    }
+
+    public String createKeyPairEC2( final String _regionStr, final String _MySSHKeyName ) throws Exception {
+        // https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/java/example_code/ec2/src/main/java/aws/example/ec2/CreateKeyPair.java
+        // http://docs.amazonaws.cn/en_us/sdk-for-java/v1/developer-guide/examples-ec2-key-pairs.html
+        final String HDR = CLASSNAME +"createKeyPairEC2("+ _regionStr +","+ _MySSHKeyName +"): ";
+        final AmazonEC2 ec2 = this.getAWSEC2Hndl( _regionStr );
+        // final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().build();
+        final CreateKeyPairRequest request = new CreateKeyPairRequest().withKeyName( _MySSHKeyName );
+        final CreateKeyPairResult response = ec2.createKeyPair(request);
+        if (this.verbose) System.out.println( HDR +"CreateKeyPairResult =\n"+ response.getKeyPair().toString() );
+        // response.getKeyPair().toString().. .. is JSON containing 3 elements: {KeyFingerprint: 29:cf:7b:f2:19:30:59:4f:96:1a:cc:3c:4d:de:2e:ce:d4:68:b0:79, KeyMaterial: -----BEGIN RSA PRIVATE KEY----- .. .. .. -----END RSA PRIVATE KEY-----,KeyName: <_MySSHKeyName>}
+        if ( this.verbose) System.out.println( HDR +"CreateKeyPairResult KeyFingerprint=\n"+ response.getKeyPair().getKeyFingerprint() );
+        if ( this.verbose) System.out.println( HDR +"CreateKeyPairResult KeyMaterial=\n"+ response.getKeyPair().getKeyMaterial() );
+        if ( this.verbose) System.out.println( HDR +"CreateKeyPairResult KeyName=\n"+ response.getKeyPair().getKeyName() );
+        // This above println gives EXACTLY IDENTICAL to _MySSHKeyName
+        // !!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // The following 3 lines .. to _PARSE_ the JSON __WILL__ __FAIL__, as the value of 1st sub-element contains ':' (colons)
+        // final LinkedHashMap<String, Object> jsonmap = JSONTools.JSONString2Map( this.verbose, response.getKeyPair().toString() ); <<--- !!!!!!!!!!!!!!!! NEVER WORKS !!!!!!!!!!!!!!!!!!!!
+        // if ( jsonmap.get("KeyFingerprint") == null || jsonmap.get("KeyMaterial") == null ||   !   _MySSHKeyName.equals( jsonmap.get("KeyName") ) )
+        //     throw new Exception( "FAILURE invoking AWS-SDK re: "+ HDR +"\nResponse="+response );
+        return response.getKeyPair().getKeyMaterial();
+    }
+
+    //==============================================================================
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //==============================================================================
+
     /**
      * Initialize &amp; Connect into AWS, by leveraging the AWS-credentials stored in a file called 'profile' in the current working directory from which this code is being run
      *  @param _verbose Whether you want deluge of debug-output onto System.out.
@@ -312,10 +360,12 @@ public class AWSSDK {
     public static AWSSDK AWSCmdline( final boolean _verbose )  throws FileNotFoundException, Exception
     {
         final String HDR = CLASSNAME + ": AWSCmdline(): ";
-        final String homedir = System.getProperty("user.home") +"/.aws";
+        final String homedir = System.getProperty("user.home");
         assertTrue( homedir != null );
-        // final String AWSProfileFileNameStr = homedir +"/profile";
-        final Path AWSProfileFilePath = FileSystems.getDefault().getPath( homedir, "profile" );
+        final File awsuserhome = new File( homedir +"/.aws" );
+        awsuserhome.mkdirs();
+        // final String AWSProfileFileNameStr = homedir +"/.aws/profile";
+        final Path AWSProfileFilePath = FileSystems.getDefault().getPath( homedir, "/.aws/profile" );
         final String AWSProfileFileNameStr = AWSProfileFilePath.toString();
 
         try {
@@ -375,11 +425,11 @@ public class AWSSDK {
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     //==============================================================================
 
-    private void  CLIPBOARD( final String _regionStr ) {
-        // https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_Region.html
-        final Region myRegion = new Region().withRegionName( _regionStr );
-        // AmazonEC2Client.serviceMetadata().regions().forEach(System.out::println);
-    }
+    // private void  CLIPBOARD( final String _regionStr ) {
+    //     // https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_Region.html
+    //     final Region myRegion = new Region().withRegionName( _regionStr );
+    //     // AmazonEC2Client.serviceMetadata().regions().forEach(System.out::println);
+    // }
 
     public static void main(String[] args) {
         try {
